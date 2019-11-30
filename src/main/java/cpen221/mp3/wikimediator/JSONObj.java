7d4b4e0f091ec94d7e9ac4f33707f2738852a033
cxsong1 <csca12.cs@gmail.com>
2019-11-29 16:18:35 -0800

    RI for JSONObj

--- src/main/java/cpen221/mp3/wikimediator/JSONObj.java
+++ src/main/java/cpen221/mp3/wikimediator/JSONObj.java
@@ -7,8 +7,13 @@ import org.json.JSONObject;
  * Abstraction Function:
  *    item is a JSONObject containing a String id representing the title of the Wikipedia page
  *    and a String containing all the text that is contained in the Wikipedia page with the
- *    title of 'id'.
+ *    title of 'id'
+ *
  * Representation Invariant:
+ *    item is not null
+ *    Strings pageTitle and pageText are not null or empty
+ *    pageTitle refers to the exact title of an existing Wikipedia page
+ *
  */
 class JSONObj implements Cacheable {
 
